2a593203bac662cfa7f41b85f8df66c55cf83327
bshahata <117234613+bshahata@users.noreply.github.com>
2023-05-01 05:10:41 -0400

    Add files via upload

---
+++ Develop/README.md
@@ -0,0 +1,28 @@
+
+# Sequel Ecommerce
+
+Sequelized E-Commerce Back-End is a program that a retail company can use to store their products and categories in a database. It helps to keep track of all the product-related information in one place.
+## 🚀 About Me
+I'm a future full stack developer in training...
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
+
+
+
+## Authors
+
+- [@bshahata](https://www.github.com/bshahata)
+
+
+## Contributing
+
+Please no Contributions 
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+Free# EcommerceBacky
---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+try {
+  
+  const categoryData = await Category.findAll({
+    
+    // include proudct model
+    include: [{ model: Product }]
+  });
+
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with thsi id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+ // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: { id: req.params.id }
+      })
+      if (!categoryData) {
+        req.status(404).json({ message: 'No category found with that id.'})
+      }
+      res.status(200).json("category updated");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id.' });
+      return;
+    }
+    res.status(200).json('category deleted!');
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
